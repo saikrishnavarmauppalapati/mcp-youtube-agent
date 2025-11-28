35a29fede7635841720defc2dd24cfb0af1c324f
sai krishna varma <101264666+saikrishnavarmauppalapati@users.noreply.github.com>
2025-11-28 18:50:08 +0530

    Update mcp_server.py

--- mcp_server.py
+++ mcp_server.py
@@ -15,8 +15,7 @@ app = FastAPI(title="MCP YouTube Agent")
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow-origin:["https://youtube-mcp-agent-frontend.vercel.app"]
-    allow-credentials: true
+    allow_origins=["https://youtube-mcp-agent-frontend.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
